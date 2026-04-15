b3c15daf65ba5005447467f56a0ef7dbc20cf485
Rahul Reddy Kuthuru <rahulreddy.10850056@ltimindtree.com>
2026-04-15 15:49:40 +0530

    Initial commit

---
+++ payPilot/src/main/java/com/skillForge/payPilot/PayPilotApplication.java
@@ -0,0 +1,13 @@
+package com.skillForge.payPilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPilotApplication.class, args);
+	}
+
+}
---
+++ payPilot/src/test/java/com/skillForge/payPilot/PayPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillForge.payPilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
